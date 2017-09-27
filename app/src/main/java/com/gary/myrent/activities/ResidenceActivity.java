ab8eae2942e00bc871aaa938400fada7d5c960f8
Gaz1676 <gazfleming@gmail.com>
2017-09-27 15:14:16 +0100

    Introduced 2 new fields: checkbox & button to access the new widgets
     Initialized:  dateButton  = (Button) findViewById(R.id.registration_date);
                          dateButton.setEnabled(false);
                          rented      = (CheckBox) findViewById(R.id.isrented);
                          rented.setOnCheckedChangeListener(this);
    
    Disabled the date button when the activity is created
    
     Implemented the OnCheckedChangeListener interface which in turn created onCheckedChanged method

--- app/src/main/java/com/gary/myrent/activities/ResidenceActivity.java
+++ app/src/main/java/com/gary/myrent/activities/ResidenceActivity.java
@@ -4,15 +4,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
 
 import com.gary.myrent.R;
 import com.gary.myrent.models.Residence;
 
-public class ResidenceActivity extends AppCompatActivity implements TextWatcher {
+
+public class ResidenceActivity extends AppCompatActivity implements TextWatcher, OnCheckedChangeListener{
 
     private EditText geolocation;
     private Residence residence;
+    private CheckBox rented;
+    private Button dateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +28,15 @@ public class ResidenceActivity extends AppCompatActivity implements TextWatcher 
         setContentView(R.layout.activity_residence);
 
         geolocation = (EditText) findViewById(R.id.geolocation);
-        residence = new Residence();
-
-        // Register a TextWatcher in the EditText geolocation object
+        residence   = new Residence();
         geolocation.addTextChangedListener(this);
+
+        dateButton  = (Button)   findViewById(R.id.registration_date);
+        dateButton.setEnabled(false);
+
+        rented = (CheckBox) findViewById(R.id.isrented);
+        rented.setOnCheckedChangeListener(this);
+
     }
 
     @Override
@@ -39,5 +52,11 @@ public class ResidenceActivity extends AppCompatActivity implements TextWatcher 
     @Override
     public void afterTextChanged(Editable editable) {
         residence.setGeolocation(editable.toString());
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+        Log.i(this.getClass().getSimpleName(), "rented Checked");
+        residence.rented = isChecked;
     }
 }
