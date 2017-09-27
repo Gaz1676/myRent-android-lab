3082ffc5a2abdd4b8c2abffcdc8ad0686e14067e
Gaz1676 <gazfleming@gmail.com>
2017-09-27 12:01:35 +0100

    Added new imports and implemented new listener interface TextWatcher Introduced a reference to the EditText field & the model object Created new methods to coincide with the introduction of the new listener interface TextWatcher

--- app/src/main/java/com/gary/myrent/MyRentActivity.java
+++ app/src/main/java/com/gary/myrent/MyRentActivity.java
@@ -2,12 +2,39 @@ package com.gary.myrent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
-public class MyRentActivity extends AppCompatActivity {
+public class MyRentActivity extends AppCompatActivity implements TextWatcher {
+
+    private EditText geolocation;
+    private Residence residence;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_rent);
+
+        geolocation = (EditText) findViewById(R.id.geolocation);
+        residence = new Residence();
+
+        // Register a TextWatcher in the EditText geolocation object
+        geolocation.addTextChangedListener(this);
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+        residence.setGeolocation(editable.toString());
     }
 }
