46ba2f0b019bd6b5a0361fcc95d8ed9c8bf65520
Gaz1676 <gazfleming@gmail.com>
2017-10-04 13:15:49 +0100

    To support enhanced navigation, we define an additional Helper method in the IntentHelper class

--- app/src/main/java/com/gary/android/helpers/IntentHelper.java
+++ app/src/main/java/com/gary/android/helpers/IntentHelper.java
@@ -1,28 +1,32 @@
 package com.gary.android.helpers;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.support.v4.app.NavUtils;
 
 import java.io.Serializable;
 
-public class IntentHelper
-{
-    public static void startActivity (Activity parent, Class classname)
-    {
+public class IntentHelper {
+    public static void startActivity(Activity parent, Class classname) {
         Intent intent = new Intent(parent, classname);
         parent.startActivity(intent);
     }
 
-    public static void startActivityWithData (Activity parent, Class classname, String extraID, Serializable extraData)
-    {
+    public static void startActivityWithData(Activity parent, Class classname, String extraID, Serializable extraData) {
         Intent intent = new Intent(parent, classname);
         intent.putExtra(extraID, extraData);
         parent.startActivity(intent);
     }
 
-    public static void startActivityWithDataForResult (Activity parent, Class classname, String extraID, Serializable extraData, int idForResult)
-    {
+    public static void startActivityWithDataForResult(Activity parent, Class classname, String extraID, Serializable extraData, int idForResult) {
         Intent intent = new Intent(parent, classname);
         intent.putExtra(extraID, extraData);
         parent.startActivityForResult(intent, idForResult);
+    }
+
+    // to support enhanced navigation, we define an additional Helper method in the IntentHelper class
+    public static void navigateUp(Activity parent) {
+        Intent upIntent = NavUtils.getParentActivityIntent(parent);
+        NavUtils.navigateUpTo(parent, upIntent);
     }
 }
