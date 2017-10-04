a50faed8f88e414b1e00bb80f1c0b0e22fa3f57b
Gaz1676 <gazfleming@gmail.com>
2017-10-04 12:13:34 +0100

    Created a new file to help with the code responding with menu selection on ResidenceListActivity

---
+++ app/src/main/java/com/gary/android/helpers/IntentHelper.java
@@ -0,0 +1,28 @@
+package com.gary.android.helpers;
+import android.app.Activity;
+import android.content.Intent;
+
+import java.io.Serializable;
+
+public class IntentHelper
+{
+    public static void startActivity (Activity parent, Class classname)
+    {
+        Intent intent = new Intent(parent, classname);
+        parent.startActivity(intent);
+    }
+
+    public static void startActivityWithData (Activity parent, Class classname, String extraID, Serializable extraData)
+    {
+        Intent intent = new Intent(parent, classname);
+        intent.putExtra(extraID, extraData);
+        parent.startActivity(intent);
+    }
+
+    public static void startActivityWithDataForResult (Activity parent, Class classname, String extraID, Serializable extraData, int idForResult)
+    {
+        Intent intent = new Intent(parent, classname);
+        intent.putExtra(extraID, extraData);
+        parent.startActivityForResult(intent, idForResult);
+    }
+}
