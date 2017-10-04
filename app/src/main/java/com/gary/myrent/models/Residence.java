113c6ba45e655fa00938dd864189afbb4b2e472e
Gaz1676 <gazfleming@gmail.com>
2017-10-04 12:37:12 +0100

    Define classes with appropriate names for each of the fields we wish to serialize Created new constructor to load a residence object from JSON Created new constructor to save object to JSON

--- app/src/main/java/com/gary/myrent/models/Residence.java
+++ app/src/main/java/com/gary/myrent/models/Residence.java
@@ -1,26 +1,53 @@
 package com.gary.myrent.models;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.Date;
 import java.util.Random;
 
-public class Residence
-{
+public class Residence {
     public Long id;
     public Long date;
 
     //a latitude longitude pair
     //example "52.4566,-6.5444"
-    private String geolocation;
+    public String geolocation;
     public boolean rented;
 
-    public Residence()
-    {
+    //define classes with appropriate names for each of the fields we wish to serialize
+    private static final String JSON_ID = "id";
+    private static final String JSON_GEOLOCATION = "geolocation";
+    private static final String JSON_DATE = "date";
+    private static final String JSON_RENTED = "rented";
+
+    public Residence() {
         id = unsignedLong();
         date = new Date().getTime();
+        geolocation = "52.253456,-7.187162";
+    }
+
+    //new constructor to load a Residence object from JSON
+    public Residence(JSONObject json) throws JSONException {
+        id = json.getLong(JSON_ID);
+        geolocation = json.getString(JSON_GEOLOCATION);
+        date = json.getLong(JSON_DATE);
+        rented = json.getBoolean(JSON_RENTED);
+    }
+
+    //a corresponding method to save an object to JSON
+    public JSONObject toJSON() throws JSONException {
+        JSONObject json = new JSONObject();
+        json.put(JSON_ID, Long.toString(id));
+        json.put(JSON_GEOLOCATION, geolocation);
+        json.put(JSON_DATE, date);
+        json.put(JSON_RENTED, rented);
+        return json;
     }
 
     /**
      * Generate a long greater than zero
+     *
      * @return Unsigned Long value greater than zero
      */
     private Long unsignedLong() {
@@ -31,13 +58,11 @@ public class Residence
         return rndVal;
     }
 
-    public void setGeolocation(String geolocation)
-    {
+    public void setGeolocation(String geolocation) {
         this.geolocation = geolocation;
     }
 
-    public String getGeolocation()
-    {
+    public String getGeolocation() {
         return geolocation;
     }
 
