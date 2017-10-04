d5472e668821116e510042430f2e131292d2fe8d
Gaz1676 <gazfleming@gmail.com>
2017-10-04 12:32:05 +0100

    Created new file

---
+++ app/src/main/java/com/gary/myrent/app/MyRentApp.java
@@ -0,0 +1,28 @@
+package com.gary.myrent.app;
+
+import android.app.Application;
+
+import com.gary.myrent.models.Portfolio;
+import com.gary.myrent.models.PortfolioSerializer;
+
+import static com.gary.android.helpers.LogHelpers.info;
+
+public class MyRentApp extends Application
+{
+    // maintains list of residences
+    public Portfolio portfolio;
+
+    // holds the file name we will use to store the portfolio
+    private static final String FILENAME = "portfolio.json";
+
+
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+        PortfolioSerializer serializer = new PortfolioSerializer(this, FILENAME);
+        portfolio = new Portfolio(serializer);
+
+        info(this, "MyRent app launched");
+    }
+}
