8efbde96b3398e91561f48547c94ee3163352b7c
Gaz1676 <gazfleming@gmail.com>
2017-10-06 16:03:58 +0100

    Created a new method to generate the contents of the email

--- app/src/main/java/com/gary/myrent/models/Residence.java
+++ app/src/main/java/com/gary/myrent/models/Residence.java
@@ -1,5 +1,9 @@
 package com.gary.myrent.models;
 
+import android.content.Context;
+
+import com.gary.myrent.R;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -14,17 +18,20 @@ public class Residence {
     //example "52.4566,-6.5444"
     public String geolocation;
     public boolean rented;
+    public String tenant;
 
     //define classes with appropriate names for each of the fields we wish to serialize
     private static final String JSON_ID = "id";
     private static final String JSON_GEOLOCATION = "geolocation";
     private static final String JSON_DATE = "date";
     private static final String JSON_RENTED = "rented";
+    private static final String JSON_TENANT = "tenant";
 
     public Residence() {
         id = unsignedLong();
         date = new Date().getTime();
         geolocation = "52.253456,-7.187162";
+        tenant = ":none presently"; // string literal to avoid a future null pointer exception error
     }
 
     //new constructor to load a Residence object from JSON
@@ -42,7 +49,31 @@ public class Residence {
         json.put(JSON_GEOLOCATION, geolocation);
         json.put(JSON_DATE, date);
         json.put(JSON_RENTED, rented);
+        json.put(JSON_TENANT, tenant);
         return json;
+    }
+
+    // a new method to generate the contents of the email
+
+    public String getResidenceReport(Context context) {
+        String rentedString = "";
+        if (rented) {
+            rentedString = context.getString(R.string.residence_report_rented);
+        }
+        else {
+            rentedString = context.getString(R.string.residence_report_not_rented);
+        }
+
+        String prospectiveTenant = tenant;
+        if (tenant == null) {
+            prospectiveTenant = context.getString(R.string.residence_report_nobody_interested);
+        }
+        else {
+            prospectiveTenant = context.getString(R.string.residence_report_prospective_tenant, tenant);
+        }
+        String report = "Location " + geolocation + " Date: " + dateString() + " " + rentedString + " " + prospectiveTenant;
+        return report;
+
     }
 
     /**
