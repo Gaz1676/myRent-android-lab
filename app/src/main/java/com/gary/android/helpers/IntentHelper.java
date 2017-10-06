460c2ce1c50588469764501f5c6fded54ecf2cb1
Gaz1676 <gazfleming@gmail.com>
2017-10-06 16:02:03 +0100

    Created  a new method to trigger contact list and email access

--- app/src/main/java/com/gary/android/helpers/IntentHelper.java
+++ app/src/main/java/com/gary/android/helpers/IntentHelper.java
@@ -2,10 +2,12 @@ package com.gary.android.helpers;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v4.app.NavUtils;
 
 import java.io.Serializable;
 
+// useful location for methods that involved intents, either explicit or implicit.
 public class IntentHelper {
     public static void startActivity(Activity parent, Class classname) {
         Intent intent = new Intent(parent, classname);
@@ -28,5 +30,12 @@ public class IntentHelper {
     public static void navigateUp(Activity parent) {
         Intent upIntent = NavUtils.getParentActivityIntent(parent);
         NavUtils.navigateUpTo(parent, upIntent);
+    }
+
+    // here we introduce a new method to trigger contact list and email access
+    public static void selectContact(Activity parent, int id)
+    {
+        Intent selectContactIntent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+        parent.startActivityForResult(selectContactIntent, id);
     }
 }
