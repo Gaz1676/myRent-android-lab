5d3e245ea0998ca8a091c03a9049aba8f4c735b7
Gaz1676 <gazfleming@gmail.com>
2017-10-06 17:25:15 +0100

    Added new declaration field MyRentApp app, initialized it and provided a getter for it

--- app/src/main/java/com/gary/myrent/app/MyRentApp.java
+++ app/src/main/java/com/gary/myrent/app/MyRentApp.java
@@ -15,6 +15,9 @@ public class MyRentApp extends Application
     // holds the file name we will use to store the portfolio
     private static final String FILENAME = "portfolio.json";
 
+    // declared a protected MyRentApp field
+    protected static MyRentApp app;
+
 
     @Override
     public void onCreate()
@@ -22,7 +25,12 @@ public class MyRentApp extends Application
         super.onCreate();
         PortfolioSerializer serializer = new PortfolioSerializer(this, FILENAME);
         portfolio = new Portfolio(serializer);
-
+        app = this; //  initialized MyRentApp app here
         info(this, "MyRent app launched");
+    }
+
+    // provides a getter for MyRentApp app
+    public static MyRentApp getApp(){
+        return app;
     }
 }
